f430913e9d208980cc88492a2e9c83a101da9275
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2023-01-19 10:43:30 +0500

    sd

--- src/Pages/PageRoutes.jsx
+++ src/Pages/PageRoutes.jsx
@@ -1,18 +1,15 @@
-import React, { Suspense } from "react";
+import React  from "react";
 import { Routes } from "react-router-dom";
 import protectedRoutes from "./ProtectedRoutes";
-import Loader from "../Components/Loader/Loader";
 import Navbar from "../Components/Navbar/Navbar";
 
 const PageRoutes = () => {
   return (
     <section className="w-full h-screen overflow-hidden bg-neutral-200">
       <Navbar />
-      <Suspense fallback={<Loader />}>
         <div className="container m-auto">
           <Routes>{protectedRoutes()}</Routes>
         </div>
-      </Suspense>
     </section>
   );
 };
