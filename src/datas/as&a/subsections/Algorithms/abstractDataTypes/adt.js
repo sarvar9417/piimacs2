6fa474df10c7517a31a4ad340dca845b56fb60d2
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2023-01-27 06:40:55 +0500

    Linked list

--- src/datas/as&a/subsections/Algorithms/abstractDataTypes/adt.js
+++ src/datas/as&a/subsections/Algorithms/abstractDataTypes/adt.js
@@ -5,12 +5,26 @@ export const adt = {
       item: "",
       defination: (
         <>
-            <b>An abstract data type</b> is a conceptual model that describes how data is
-          organised and which operations can be carried out on the data (e.g.
-          insert, delete) from the perspective of an end user who does not need
-          to know how this is implemented. There are often many ways to
-          implement an abstract data type, depending on the programming language
-          being used.
+          <b>Ma’lumotlar tuzilmasi (data structures)</b> — bu ma’lumotlarni
+          samarali o’qish va o’zgartirish imkonini beruvchi, ma’lumotlarni
+          saqlash va boshqarishning bir formatga solingan shaklidir. Eng sodda
+          ma’lumotlar tuzilmasiga misol qilib massiv (array)ni ko’rsatishimiz
+          mumkin.
+          <br />
+          <br />
+          Data structures primitiv va non-primitiv turga ajratiladi.
+          <br />
+          <br />
+          <ul className="pl-7 list-disc">
+            <li>Primitiv turga Integer, Boolean, Text, Character kiradi.</li>
+            <li>
+              Non-primitiv turning o’zi ikkiga bo’linadi:
+              <ul className="pl-14 list-item ">
+                <li>Chiziqli: Array, Linked List, Stack, Queues</li>
+                <li> Chiziqsiz: Trees, Graphs.</li>
+              </ul>
+            </li>
+          </ul>
         </>
       ),
     },
--- src/datas/as&a/subsections/Algorithms/abstractDataTypes/linkedList.js
+++ src/datas/as&a/subsections/Algorithms/abstractDataTypes/linkedList.js
@@ -1,8 +1,129 @@
+import linked from "../../../../../assets/images/linked-list.png";
 export const linkedList = {
   name: "Linked list",
   definations: [
     {
-      defination: <>LinkedList</>,
+      defination: (
+        <>
+          Linked list — bu tugunlardan ya’ni Node’lardan iborat chiziqli tuzilma
+          bo’lib, har bir node o’zida elementni va keyingi (ba’zan oldingi ham)
+          node adresini ko’rsatuvchi ko’rsatkichni (pointerni) saqlaydi.
+          <br />
+          <br />
+          Dastlabki Node head deb ataladi. Head’dan keyingi node’lar o’zidan
+          oldingi node’ning pointeri’ga bog’langan bo’ladi. Demak biz dastlabki
+          node, ya’ni head’ni bilgan holda, pointerlar yordamida keyingi
+          node’larga o’tib kerakli elementni topamiz. Array’da index nomerini
+          bilib, darhol arr[index] bilan elementni o’qib olgan bo’lsak, Linked
+          listda o’sha elementga borish uchun ungacha bo’lgan barcha
+          elementlardan o’tib kelish kerak.
+          <br />
+          <br />
+        </>
+      ),
+
+      image: {
+        name: linked,
+        alt: "Loop",
+        width: "600px",
+      },
+    },
+    {
+      name: "Linked list ustida bajariladigan asosiy amallar",
+      defination: (
+        <>
+          <b>Ro’yhat boshiga element qo’shish (insertAtHead). O(1) vaqtda</b>
+          <br />
+          Yangi node qo’shilib, uning pointeriga linked list head’i ko’rsatib
+          qo’yiladi. <br /> <br />
+          <b>Ro’yhat oxiriga element qo’shish (insertAtEnd). O(1) vaqtda</b>
+          <br />
+          Yangi node qo’shiladi. Uni linked list ohiriga qo’shish uchun oldin
+          pointer = null bo’lgan ohirini topib olinadi. Keyin pointer yangi
+          node’ga ko’rsatib qo’yiladi. <br /> <br />
+          <b>Ma’lum bir elementni o’chirish (Delete). O(1) vaqtda</b>
+          <br />
+          Buning uchun linked list ichidan element topiladi. Agar element
+          o’rtada bo’lsa Undan oldingi elementning pointeri undan keyingi
+          elementga ulab qo’yiladi. Ohirida bo’lsa, oldingi elementning pointeri
+          bo’shatiladi. <br /> <br />
+          <b>
+            Ma’lum bir elementni o’qib olish. Qidirish (Search). O(n) vaqtda
+          </b>{" "}
+          <br />
+          Elementni qidirish har doim linked list boshidan boshlanadi.
+        </>
+      ),
+    },
+    {
+      defination: (
+        <>
+          <b>Linked listni e'lon qilish</b>
+        </>
+      ),
+      pseudocode:
+        "DECLARE myLinkedList Array[0:11] OF INTEGER\n" +
+        "DECLARE myLinkedListPointers Array[0:11] OF INTEGER\n" +
+        "DECLATE startPointer: INTEGER\n" +
+        "DECLARE heapStartPointer: INTEGER\n" +
+        "DECLARE index: INTEGER\n" +
+        "heapPointer <- 0\n" +
+        "startPointer <- -1\n" +
+        "FOR index <- 0 TO 11\n" +
+        "    myLinkedListPointers[index] <- index + 1\n" +
+        "NEXT index\n" +
+        "myLinkedListPointers[index] <- -1",
+      code:
+        "myLinkedList = ['' for i in range(12)]\n" +
+        "myLinkedListPointers = [index for index in range(11)]\n" +
+        "myLinkedListPointers  = -1\n" +
+        "startPointer = -1\n" +
+        "heapStartPointer = 0",
+    },
+
+    {
+      defination: <>Linked listdan kiritilgan sonni qidirish algoritmi.</>,
+
+      pseudocode:
+        "numbers <- [6, 7, 3, 9, 2, 4, 1, 0, 5, 8]\n" +
+        "NextPointer <- [1, 9, 5, -1, 2, 8, 4, 6, 0, 3]\n" +
+        "HeadPointer <- 4\n" +
+        "Pointer <- HeadPointer \n" +
+        "Found <- -1\n" +
+        'OUTPUT "Enter a searching number "\n' +
+        "INPUT searchingNumber\n" +
+        "WHILE Pointer <> -1\n" +
+        "    IF Numbers[Pointer] = searchingNumber THEN \n" +
+        "        Found <- Pointer\n" +
+        "        Pointer <- -1\n" +
+        "    ELSE\n" +
+        "        Pointer <- NextPointer[Pointer] \n" +
+        "    ENDIF\n" +
+        "ENDWHILE\n" +
+        "IF Found <> -1 THEN\n" +
+        '   OUTPUT Found, " is found"\n' +
+        "ELSE\n" +
+        '   OUTPUT "The number you wanted is not in the list"\n' +
+        "ENDIF",
+
+      code:
+        "numbers =     [6, 7, 3, 9, 2, 4, 1, 0, 5, 8]\n" +
+        "nextPointer = [1, 9, 5, -1, 2, 8, 4, 6, 0, 3]\n" +
+        "headPointer = 7\n" +
+        "pointer = headPointer \n" +
+        "found = -1\n" +
+        'searchingNumber = int(input("Enter a searching number "))\n' +
+        "while (pointer != -1):\n" +
+        "    print(pointer)\n" +
+        "    if numbers[pointer] == searchingNumber: \n" +
+        "        found = pointer\n" +
+        "        pointer = -1\n" +
+        "    else:\n" +
+        "        pointer = nextPointer[pointer] \n" +
+        "if found != -1:\n" +
+        "   print(found)\n" +
+        "else:\n" +
+        '   print("The number you wanted is not in the list")',
     },
     {
       name: "Tasks",
@@ -21,19 +142,19 @@ export const linkedList = {
         </>
       ),
     },
-      {
-          defination: (
-              <>
-                  <b>3. (9618/21/M/J/21:6)</b>
-              </>
-          ),
-      },
-      {
-          defination: (
-              <>
-                  <b>4. (9618/41/M/J/21:1)</b>
-              </>
-          ),
-      },
+    {
+      defination: (
+        <>
+          <b>3. (9618/21/M/J/21:6)</b>
+        </>
+      ),
+    },
+    {
+      defination: (
+        <>
+          <b>4. (9618/41/M/J/21:1)</b>
+        </>
+      ),
+    },
   ],
 };
