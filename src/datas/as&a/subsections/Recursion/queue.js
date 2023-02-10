0c1a2b952f8027c8d845ed084781406b28b7b8d2
unknown <sarvar9417@gmail.com>
2023-02-10 09:23:04 +0500

    casc

--- src/datas/as&a/subsections/Recursion/queue.js
+++ src/datas/as&a/subsections/Recursion/queue.js
@@ -1,9 +1,25 @@
 export const queue = {
-    name: 'Insertion Sort',
+    name: 'Search item in queue',
     definations:[
         {
             pseudocode:"",
-            code:""
+            code:"def searchQueue(startPointer, searchItem):\n" +
+                "    if (startPointer == len(a)):\n" +
+                "        return -1\n" +
+                "    elif (a[startPointer] == searchItem ):\n" +
+                "        return startPointer\n" +
+                "    else:\n" +
+                "        a[startPointer] = ''\n" +
+                "        return searchQueue(startPointer + 1, searchItem)\n" +
+                "        \n" +
+                "a = [1, 9, 8,  6, 3]\n" +
+                "startPointer = 0\n" +
+                "n = int(input(\"Please enter search item: \"))\n" +
+                "f = searchQueue(startPointer, n)\n" +
+                "if f == -1:\n" +
+                "    print(\"Item not found\")\n" +
+                "else:\n" +
+                "    print(\"Item index\", f)",
         }
     ]
 }
--- src/datas/as&a/subsections/Recursion/stack.js
+++ src/datas/as&a/subsections/Recursion/stack.js
@@ -1,9 +1,25 @@
 export const stack = {
-    name: 'Insertion Sort',
+    name: 'Search item in stack',
     definations:[
         {
             pseudocode:"",
-            code:""
+            code:"def searchStack(topPointer, searchItem):\n" +
+                "    if (topPointer == -1):\n" +
+                "        return topPointer\n" +
+                "    elif (a[topPointer] == searchItem ):\n" +
+                "        return topPointer\n" +
+                "    else:\n" +
+                "        a.pop()\n" +
+                "        return searchStack(topPointer - 1, searchItem)\n" +
+                "        \n" +
+                "a = [1, 9, 8,  6, 3]\n" +
+                "topPointer = 4\n" +
+                "n = int(input(\"Please enter search item: \"))\n" +
+                "f = searchStack(topPointer, n)\n" +
+                "if f == -1:\n" +
+                "    print(\"Item not found\")\n" +
+                "else:\n" +
+                "    print(\"Item index\", f)"
         }
     ]
 }
